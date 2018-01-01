b25f81dfb1471f228b8a718da47ebfbdd436ae07
mgh3326 <mgh3326@naver.com>
2018-01-01 15:40:41 +0900

    complete

--- src/ReverseStr.java
+++ src/ReverseStr.java
@@ -1,15 +1,22 @@
 public class ReverseStr {
     public String reverseStr(String str){
         char[] arr=new char[str.length()];
-        arr[0]=str.charAt(0);
-        for(int i=1;i<str.length();i++)
+        for(int i=0;i<str.length();i++)
         {
-            if(arr[0]<str.charAt(i))
-            {
+            arr[i]=str.charAt(i);
 
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
-        return "";
+        str = String.valueOf(arr);
+        return str;
     }
 
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
@@ -24,3 +31,5 @@ public class ReverseStr {
 //        str에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 String을 리턴해주세요.
 //        str는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
 //        예를들어 str이 "Zbcdefg"면 "gfedcbZ"을 리턴하면 됩니다.
+
+//http://blog.naver.com/PostView.nhn?blogId=jin93hj&logNo=220578168789&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView
